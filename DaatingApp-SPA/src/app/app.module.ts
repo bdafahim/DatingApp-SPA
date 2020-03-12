2af408f7fdbe97298b58b769ab84054b4b4e7c64
bdafahim <badrudduza20@student.sust.edu>
2020-03-12 23:09:11 +0600

    first api request

--- DaatingApp-SPA/src/app/app.module.ts
+++ DaatingApp-SPA/src/app/app.module.ts
@@ -1,20 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
+import { AppComponent } from "./app.component";
+import { ValueComponent } from "./value/value.component";
+import { from } from "rxjs";
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      ValueComponent
-   ],
-   imports: [
-      BrowserModule
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [AppComponent, ValueComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
