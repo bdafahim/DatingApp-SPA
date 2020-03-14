71fec718c28c90871f6296d19576d5d53956a197
bdafahim <badrudduza20@student.sust.edu>
2020-03-14 21:05:35 +0600

    Navbar and Login form

--- DaatingApp-SPA/src/app/app.module.ts
+++ DaatingApp-SPA/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { ValueComponent } from "./value/value.component";
 import { from } from "rxjs";
+import { NavComponent } from "./nav/nav.component";
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, ValueComponent, NavComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
