739896029a4c8021d07b562fbf8346c9beabbe5f
bdafahim <badrudduza20@student.sust.edu>
2020-03-18 12:55:06 +0600

    Components created and Routing

--- DaatingApp-SPA/src/app/app.module.ts
+++ DaatingApp-SPA/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { BsDropdownModule } from "ngx-bootstrap";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { from } from "rxjs";
@@ -11,15 +12,30 @@ import { NavComponent } from "./nav/nav.component";
 import { AuthService } from "./_services/auth.service";
 import { HomeComponent } from "./home/home.component";
 import { RegisterComponent } from "./register/register.component";
+import { MemberListComponent } from "./member-list/member-list.component";
+import { ListComponent } from "./list/list.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { ListsComponent } from "./lists/lists.component";
+import { appRoutes } from "./routes";
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    MemberListComponent,
+    ListComponent,
+    MessagesComponent,
+    ListsComponent
+  ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     BsDropdownModule.forRoot(),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
