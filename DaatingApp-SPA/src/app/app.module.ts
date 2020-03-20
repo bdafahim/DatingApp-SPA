6e160c11850829120045b37128fa5221047701dd
bdafahim <badrudduza20@student.sust.edu>
2020-03-20 23:39:46 +0600

    Member Card to display users

--- DaatingApp-SPA/src/app/app.module.ts
+++ DaatingApp-SPA/src/app/app.module.ts
@@ -12,10 +12,11 @@ import { NavComponent } from "./nav/nav.component";
 import { AuthService } from "./_services/auth.service";
 import { HomeComponent } from "./home/home.component";
 import { RegisterComponent } from "./register/register.component";
-import { MemberListComponent } from "./member-list/member-list.component";
+import { MemberListComponent } from "./members/member-list/member-list.component";
 import { MessagesComponent } from "./messages/messages.component";
 import { ListsComponent } from "./lists/lists.component";
 import { appRoutes } from "./routes";
+import { MemberCardComponent } from "./members/member-card/member-card.component";
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { appRoutes } from "./routes";
     RegisterComponent,
     MemberListComponent,
     MessagesComponent,
-    ListsComponent
+    ListsComponent,
+    MemberCardComponent
   ],
   imports: [
     BrowserModule,
--- DaatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DaatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { User } from "../_models/user";
-import { UserService } from "../_services/user.service";
-import { AlertifyService } from "../_services/alertify.service";
+import { User } from "src/app/_models/user";
+import { UserService } from "src/app/_services/user.service";
+import { AlertifyService } from "src/app/_services/alertify.service";
 
 @Component({
   selector: "app-member-list",
