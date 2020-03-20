381ac6b95af07c5435f3115ef9422035578df8e1
bdafahim <badrudduza20@student.sust.edu>
2020-03-20 20:21:55 +0600

    user Service

--- DaatingApp-SPA/src/app/_services/user.service.ts
+++ DaatingApp-SPA/src/app/_services/user.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from "@angular/core";
 import { environment } from "src/environments/environment";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { User } from "../_models/user";
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: "Bearer " + localStorage.getItem("token")
+  })
+};
 
 @Injectable({
   providedIn: "root"
@@ -13,9 +19,9 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.baseUrl + "users");
+    return this.http.get<User[]>(this.baseUrl + "users", httpOptions);
   }
   getUser(id): Observable<User> {
-    return this.http.get<User>(this.baseUrl + "users/" + id);
+    return this.http.get<User>(this.baseUrl + "users/" + id, httpOptions);
   }
 }
