ad2069459a7b7a6c4d844999cf69a64af78c6d18
bdafahim <badrudduza20@student.sust.edu>
2020-03-21 23:53:58 +0600

    Photo of a memeber

--- DaatingApp-SPA/src/app/app.module.ts
+++ DaatingApp-SPA/src/app/app.module.ts
@@ -1,4 +1,8 @@
-import { BrowserModule } from "@angular/platform-browser";
+import {
+  BrowserModule,
+  HammerGestureConfig,
+  HAMMER_GESTURE_CONFIG
+} from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
--- DaatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
+++ DaatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -24,15 +24,15 @@ export class MemberDetailComponent implements OnInit {
       this.user = data["user"];
     });
   }
-
-  // loadUser() {
-  //   this.userService.getUser(+this.route.snapshot.params["id"]).subscribe(
-  //     (user: User) => {
-  //       this.user = user;
-  //     },
-  //     error => {
-  //       this.alertify.error(error);
-  //     }
-  //   );
-  // }
 }
+
+// loadUser() {
+//   this.userService.getUser(+this.route.snapshot.params["id"]).subscribe(
+//     (user: User) => {
+//       this.user = user;
+//     },
+//     error => {
+//       this.alertify.error(error);
+//     }
+//   );
+// }
