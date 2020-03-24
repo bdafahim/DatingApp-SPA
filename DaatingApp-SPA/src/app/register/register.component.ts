6dc09ed61f80df755ec2d8a7884e5c4181f023ee
bdafahim <badrudduza20@student.sust.edu>
2020-03-24 19:27:34 +0600

    custom validator to match password and confirm password

--- DaatingApp-SPA/src/app/register/register.component.ts
+++ DaatingApp-SPA/src/app/register/register.component.ts
@@ -19,15 +19,24 @@ export class RegisterComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.registerForm = new FormGroup({
-      username: new FormControl("Hello", Validators.required),
-      password: new FormControl("", [
-        Validators.required,
-        Validators.minLength(4),
-        Validators.maxLength(8)
-      ]),
-      confirmPassword: new FormControl("", Validators.required)
-    });
+    this.registerForm = new FormGroup(
+      {
+        username: new FormControl("Hello", Validators.required),
+        password: new FormControl("", [
+          Validators.required,
+          Validators.minLength(4),
+          Validators.maxLength(8)
+        ]),
+        confirmPassword: new FormControl("", Validators.required)
+      },
+      this.passwordMatchValidator
+    );
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get("password").value === g.get("confirmPassword").value
+      ? null
+      : { mismatch: true };
   }
 
   register() {
