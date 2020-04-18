3ee183c818862f1817000a7ffbe456bd82f839a1
bdafahim <badrudduza20@student.sust.edu>
2020-04-18 11:29:43 +0600

    Delete photo functionality done in SPA

--- DaatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DaatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -81,4 +81,23 @@ export class PhotoEditorComponent implements OnInit {
         }
       );
   }
+
+  deletePhoto(id: number) {
+    this.alertify.confirm("Are you sure you want to delete the photo", () => {
+      this.userService
+        .deletePhoto(this.authService.decodedToken.nameid, id)
+        .subscribe(
+          () => {
+            this.photos.splice(
+              this.photos.findIndex((p) => p.id === id),
+              1
+            );
+            this.alertify.success("Photos has been deleted");
+          },
+          (error) => {
+            this.alertify.error("Failed to delete the photo");
+          }
+        );
+    });
+  }
 }
