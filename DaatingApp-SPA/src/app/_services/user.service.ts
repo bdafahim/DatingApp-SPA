e5c2aff9778597cb17aa22f42d7236b85df2d964
bdafahim <badrudduza20@student.sust.edu>
2020-05-01 13:38:14 +0600

    Filtering (not completed bugs exist)

--- DaatingApp-SPA/src/app/_services/user.service.ts
+++ DaatingApp-SPA/src/app/_services/user.service.ts
@@ -14,7 +14,11 @@ export class UserService {
 
   constructor(private http: HttpClient) {}
 
-  getUsers(page?, itemsPerPage?): Observable<PaginatedResult<User[]>> {
+  getUsers(
+    page?,
+    itemsPerPage?
+    // userParams?
+  ): Observable<PaginatedResult<User[]>> {
     const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<
       User[]
     >();
@@ -25,6 +29,12 @@ export class UserService {
       params = params.append("pageNumber", page);
       params = params.append("pageSize", itemsPerPage);
     }
+
+    // if (userParams != null) {
+    //   params = params.append("minAge", userParams.minAge);
+    //   params = params.append("maxAge", userParams.maxAge);
+    //   params = params.append("gender", userParams.gender);
+    // }
 
     return this.http
       .get<User[]>(this.baseUrl + "users", { observe: "response", params })
--- DaatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DaatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -12,6 +12,13 @@ import { Pagination, PaginatedResult } from "src/app/_models/pagination";
 })
 export class MemberListComponent implements OnInit {
   users: User[];
+  // user: User = JSON.parse(localStorage.getItem("user"));
+  // genderList: [
+  //   { value: "male"; display: "Males" },
+  //   { value: "female"; display: "Females" }
+  // ];
+  // userParams: any = {};
+
   pagination: Pagination;
 
   constructor(
@@ -25,6 +32,10 @@ export class MemberListComponent implements OnInit {
       this.users = data["users"].result;
       this.pagination = data["users"].pagination;
     });
+    // this.userParams.gender = this.user.gender === "female" ? "male" : "female";
+    // this.userParams.minAge = 18;
+    // this.userParams.maxAge = 99;
+    // console.log(JSON.parse(localStorage.getItem("user")));
   }
 
   pageChanged(event: any): void {
@@ -32,9 +43,21 @@ export class MemberListComponent implements OnInit {
     this.loadUsers();
   }
 
+  // resetFilters() {
+  //   this.userParams.gender = this.user.gender === "female" ? "male" : "female";
+  //   this.userParams.minAge = 18;
+  //   this.userParams.maxAge = 99;
+  //   this.loadUsers();
+  //   console.log("Reser filter");
+  // }
+
   loadUsers() {
     this.userService
-      .getUsers(this.pagination.currentPage, this.pagination.itemsPerPage)
+      .getUsers(
+        this.pagination.currentPage,
+        this.pagination.itemsPerPage
+        // this.userParams
+      )
       .subscribe(
         (res: PaginatedResult<User[]>) => {
           this.users = res.result;
