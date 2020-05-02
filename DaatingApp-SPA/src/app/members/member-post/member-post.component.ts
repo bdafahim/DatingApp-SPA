58a3a117c77d47c199215fb026ba722d65aa6449
bdafahim <badrudduza20@student.sust.edu>
2020-05-02 16:03:02 +0600

    member-post component created

---
+++ DaatingApp-SPA/src/app/members/member-post/member-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-member-post',
+  templateUrl: './member-post.component.html',
+  styleUrls: ['./member-post.component.css']
+})
+export class MemberPostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
