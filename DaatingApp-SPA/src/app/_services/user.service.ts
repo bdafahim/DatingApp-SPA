791a178cfc9c536f97f5f742be158e5942ee61d6
bdafahim <badrudduza20@student.sust.edu>
2020-05-04 12:39:56 +0600

    Authservice for SendLike

--- DaatingApp-SPA/src/app/_services/user.service.ts
+++ DaatingApp-SPA/src/app/_services/user.service.ts
@@ -68,4 +68,10 @@ export class UserService {
   deletePhoto(userId: number, id: number) {
     return this.http.delete(this.baseUrl + "users/" + userId + "/photos/" + id);
   }
+  sendLike(id: number, recipientId: number) {
+    return this.http.post(
+      this.baseUrl + "users/" + id + "/like/" + recipientId,
+      {}
+    );
+  }
 }
